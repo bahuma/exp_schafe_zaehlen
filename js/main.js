70384a0ef57d9a9a3c6004734fdb3fe2bd8614b4
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2014-12-04 11:55:08 +0000

    Altered Sheep Path

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-var path = "M-200,323.5h340.607c0,0,138.279-1.152,213.329-93.709c75.05-92.557,185.939,10.978,271.781,51.985c85.842,41.008,247.655,42.338,270.943,41.69";
-var pathAnimator = new PathAnimator( path ),	// initiate a new pathAnimator object
-objToAnimate = document.getElementById('sheep1'),	// The object that will move along the path
+var path = "M-196,323.5h439.238c0,0,105.416,8.525,202.198-84.031c96.784-92.557,206.301-6.006,317,35c110.699,41.009,425.757,49.646,455.789,48.997";
+var sheepAnimator = new PathAnimator( path ),	// initiate a new pathAnimator object
+sheep = document.getElementById('sheep1'),	// The object that will move along the path
 speed = 3,	 		// seconds that will take going through the whole path
 reverse = false,	// go back of forward along the path
 startOffset = 0		// between 0% to 100%
 	
-pathAnimator.start( speed, step, reverse, startOffset, finish);
+sheepAnimator.start( speed, sheepStep, reverse, startOffset, sheepFinish);
 
-function step( point, angle ){
+function sheepStep( point, angle ){
 	// do something every "frame" with: point.x, point.y & angle
-	objToAnimate.style.cssText = "top:" + point.y + "px;" +
+	sheep.style.cssText = "top:" + point.y + "px;" +
 								"left:" + point.x + "px;" +
 								"transform:rotate("+ angle +"deg);" +
 								"-webkit-transform:rotate("+ angle +"deg);";
 }
 
-function finish(){
-	pathAnimator.start( speed, step, reverse, startOffset, finish);
+function sheepFinish(){
+	sheepAnimator.start( speed, sheepStep, reverse, startOffset, sheepFinish);
 }
